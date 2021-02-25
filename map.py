bfbb196fcea2ca77ddfd29289328eb75280444c4
Ludvig Sellebråten <l.sellebraten@gmail.com>
2021-02-25 11:43:38 +0100

    Added volcano.txt

--- map.py
+++ map.py
@@ -1,3 +1,24 @@
 import folium
-map = folium.Map(location=[59.242062,18.085211], zoom_start=6)
+map = folium.Map(
+    location=[38.2, -99.1], 
+    zoom_start=5,
+    tiles="Stamen Terrain",
+    # width=750, 
+    # height=500
+)
+
+fg = folium.FeatureGroup(name="My Map")
+
+for coordinates in [[38.2, -99.1],[39.2, -97.1]]:
+    fg.add_child(
+        folium.Marker(
+            location=coordinates, 
+            popup="Hi, I am a Marker", 
+            icon=folium.Icon(
+                color="green"
+            )
+        )
+    )
+
+map.add_child(fg)
 map.save("Map1.html")
