63f2763052148fadc993dbf79c336542d5668c6d
Ludvig Sellebråten <l.sellebraten@gmail.com>
2021-02-25 14:19:57 +0100

    added info from volcanoes.txt with pointer link

--- map.py
+++ map.py
@@ -1,19 +1,36 @@
 import folium
+import pandas
+
+# Check headers =>
+# data.columns
+data = pandas.read_csv("Volcanoes.txt")
+
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
+name = list(data["NAME"])
+
+html = """
+Volcano name:<br>
+<a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a><br>
+Height: %s m
+"""
+
 map = folium.Map(
     location=[38.2, -99.1], 
     zoom_start=5,
     tiles="Stamen Terrain",
-    # width=750, 
-    # height=500
+
 )
 
 fg = folium.FeatureGroup(name="My Map")
 
-for coordinates in [[38.2, -99.1],[39.2, -97.1]]:
+for lt, ln, el, nm in zip(lat, lon, elev, name):
+    iframe = folium.IFrame(html=html % (nm, nm, el), width=200, height=100)
     fg.add_child(
         folium.Marker(
-            location=coordinates, 
-            popup="Hi, I am a Marker", 
+            location=[lt,ln], 
+            popup=folium.Popup(iframe),
             icon=folium.Icon(
                 color="green"
             )
@@ -21,4 +38,4 @@ for coordinates in [[38.2, -99.1],[39.2, -97.1]]:
     )
 
 map.add_child(fg)
-map.save("Map1.html")
+map.save("MapAdv.html")
